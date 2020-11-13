0f4fc94a0854423a4039011b94abd0dfb1cd45b6
b-husein <bajrektarevic.husein@gmail.com>
2020-11-13 17:18:45 +0100

    added fallback

--- app.js
+++ app.js
@@ -4,7 +4,17 @@
 let http = require("http");
 
 let ourApp = http.createServer(function(req, res){
-    res.end("Hello and welcome!")
+    // response object
+    if(req.url == "/") {
+        res.end("Hello and welcome to our homepage!")
+    }
+    if(req.url == "/about") {
+        res.end("You can read everything about our company here.")
+    }
+    // fallback
+    res.end("We cannot find the page you're looking for.")
 });
+
+// listen method with port 3000 (localhost:3000)
 ourApp.listen(3000);
 
